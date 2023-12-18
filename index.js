306a93a16573c9e1f006fd8e9e3c64114d2d9206
Yashd07 <yashd1272@gmail.com>
2023-12-18 19:10:14 +0530

    user jwt setup

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
-
+import userRouter from './routes/users.js';
 //pass:   
 
 const app = express();
@@ -15,6 +15,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use("/user", userRouter);
 
 const CONNECTION_URL = 'mongodb+srv://yashd1272:G3x8PGALcSSDxcHq@cluster0.0wx0cys.mongodb.net/';
 const PORT = process.env.PORT|| 5000;
